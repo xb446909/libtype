0a1676853a9403340d8ee34e0a0a8d9905088e40
Xiong <xiongblw0852@hanslaser.com>
2019-11-04 17:32:00 +0800

    添加了头文件

---
+++ libtype/libtype.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void __stdcall SetText(HDC hdc, int x, int y, const char* str, const char* szFont, int nHeight, bool bBold, bool bItalic, bool bUnderline);
+int __stdcall GetBezierCount();
+int __stdcall GetBezierPointsCount(int bezierIndex);
+int __stdcall GetPolylineCount();
+int __stdcall GetPolylinePointsCount(int polylineIndex);
+int __stdcall GetLinePointsCount();
+void __stdcall GetBezierPoint(int bezierIndex, int ptIndex, int* x, int* y);
+void __stdcall GetPolylinePoint(int polylineIndex, int ptIndex, int* x, int* y);
+void __stdcall GetlinePoint(int idx, int* x1, int* y1, int* x2, int* y2);
